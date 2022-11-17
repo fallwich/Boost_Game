a422c771f5263a5290f4779a89c3e4303285810f
Glenn <fallwich@naver.com>
2022-11-18 02:35:02 +0900

    로켓에 사운드 추가

--- Boost_game/Assets/Script/Movement.cs
+++ Boost_game/Assets/Script/Movement.cs
@@ -7,11 +7,13 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
     Rigidbody rb; // rb = rigidbody
+    AudioSource audioSource;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,15 @@ public class Movement : MonoBehaviour
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); 
             //프레임율에 영향을 받지 않게 Time.deltaTime(알아서 계산 해주는 함수)을 사용
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
     }
