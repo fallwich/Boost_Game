31eef29d01301d8d8d1b1050190fad92aea4a9c4
Glenn <fallwich@naver.com>
2022-11-19 00:33:28 +0900

    Refactor With Extract Method
    Extract Method를 이용하여 가독성 높임.

--- Boost_game/Assets/Script/Movement.cs
+++ Boost_game/Assets/Script/Movement.cs
@@ -34,50 +34,69 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); 
-            //프레임율에 영향을 받지 않게 Time.deltaTime(알아서 계산 해주는 함수)을 사용
-            if(!audioSource.isPlaying) 
-            {
-                audioSource.PlayOneShot(mainEngine); //audioSource.Play는 효과음이 하나일 때 효과적이다.
-                // AudioSource.PlayOneShot은 여러개의 효과음 사용 가능.
-            }
-            if(!mainEngineParticels.isPlaying)
-            {
-                mainEngineParticels.Play();
-            }
+            StartThrusting();
         }
         else
         {
-            audioSource.Stop();
-            mainEngineParticels.Stop();
+            StopThrusting();
         }
     }
-
     void ProcessRotation()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            ApplyRotation(rotationThrust);
-            if(!rightThrusterParticels.isPlaying)
-            {
-                rightThrusterParticels.Play();
-            }
+            RotateLeft();
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            ApplyRotation(-rotationThrust);
-            if(!leftThrusterParticels.isPlaying)
-            {
-                leftThrusterParticels.Play();
-            }
+            RotateRight();
         }
         else
         {
-            rightThrusterParticels.Stop();
-            leftThrusterParticels.Stop();
+            StopRotating();
         }
     }
+    void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        //프레임율에 영향을 받지 않게 Time.deltaTime(알아서 계산 해주는 함수)을 사용
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine); //audioSource.Play는 효과음이 하나일 때 효과적이다.
+                                                 // AudioSource.PlayOneShot은 여러개의 효과음 사용 가능.
+        }
+        if (!mainEngineParticels.isPlaying)
+        {
+            mainEngineParticels.Play();
+        }
+    }
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainEngineParticels.Stop();
+    }
+    private void StopRotating()
+    {
+        rightThrusterParticels.Stop();
+        leftThrusterParticels.Stop();
+    }
 
+    private void RotateRight()
+    {
+        ApplyRotation(-rotationThrust);
+        if (!leftThrusterParticels.isPlaying)
+        {
+            leftThrusterParticels.Play();
+        }
+    }
+    private void RotateLeft()
+    {
+        ApplyRotation(rotationThrust);
+        if (!rightThrusterParticels.isPlaying)
+        {
+            rightThrusterParticels.Play();
+        }
+    }
     public void ApplyRotation(float rotationThisFrame)
     {
         rb.freezeRotation = true; // 수동 제어를 위한 회전 고정
